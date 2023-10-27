e0c3b357123af75235e59059ccf902def0b04481
Eddie Sandler <ed.sandler@gmail.com>
2023-10-27 13:00:13 -0400

    displays result on screen

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ base_url = 'http://api.exchangerate.host/convert?access_key='
 
 @app.route('/')
 def display_form():
+    '''displays form for user input'''
 
     return render_template('fx_converter.html')
 
 
 @app.route('/get_input', methods=['GET', 'POST'])
 def get_user_input():
+    '''retrieves user input and stores in Session'''
     data = {
         'from_currency': request.form.get('currency_1'),
         'to_currency': request.form.get('currency_2'),
@@ -32,48 +34,37 @@ def get_user_input():
 
 @app.route('/make_api_call/', methods=['GET'])
 def fx_conversion():
-    amount=session['data']['amount']
-    from_currency=session['data']['from_currency']
-    to_currency=session['data']['to_currency']
+    '''calls api and returns results which is saved in session'''
+    amount = session['data']['amount']
+    from_currency = session['data']['from_currency']
+    to_currency = session['data']['to_currency']
     url = f'{base_url}{access_key}&from={from_currency}&to={to_currency}&amount={amount}&format=1'
+
     response = requests.get(url=url)
+    #needs to handle KeyError
     if response.status_code == 200:
-        res = response.json()
+        result = response.json()
+        session['result'] = result
+        return redirect('/render_results/')
+    else:
+        return 'error- currency does not exist'
+
+
+@app.route('/render_results/', methods=['GET', 'POST'])
+def show_results():
+    '''displays result of currency conversion and adds currency symbol '''
+
+    symbol = c.get_symbol(session['data']['to_currency'].upper())
+
+    timestamp = session['result']["info"]["timestamp"]
+    human_readable_time = datetime.utcfromtimestamp(
+            timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    print(f'timestamp of transaction : {human_readable_time}')
+    return render_template('fx_converter.html',symbol=symbol)
 
 
 
-        print(f'you converted {amount} of {from_currency} to {to_currency}')
-        print(f"quote: {res['info']['quote']}")
-        print(f"total: {res['result']}")
-        symbol=c.get_symbol(to_currency)
-        print(f"symbol: {symbol}")
-        
-
-        print(f'The rate is {res["info"]["quote"]} ')
-
-        timestamp = res["info"]["timestamp"]
-        human_readable_time = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        print(f'timestamp of transaction : {human_readable_time}')
-        return res
-
-
-#         print(c.get_symbol('GBP'))
-#         print (c.get_symbol('EUR'))
-
-
-
-# {
-#   "info": {
-#     "quote": 0.82326,
-#     "timestamp": 1698419583
-#   },
-#   "privacy": "https://currencylayer.com/privacy",
-#   "query": {
-#     "amount": 90000000000,
-#     "from": "USD",
-#     "to": "GBP"
-#   },
-#   "result": 74093400000,
-#   "success": true,
-#   "terms": "https://currencylayer.com/terms"
-# }
+@app.route('/clear_session', methods=['GET'])
+def clear_session():
+    session.clear()
+    return redirect('/')
