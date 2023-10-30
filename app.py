60e043fceedce18ec827f1c921e7da0fe05c71e3
Eddie Sandler <ed.sandler@gmail.com>
2023-10-30 18:02:04 -0400

    code refactored

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, redirect, session, flash
-from currency_converter import make_api_call, get_currency_symbol,display_result_page
+from flask import Flask,flash, request, render_template, redirect, session
+from currency_converter import make_api_call, get_currency_symbol
 
 app = Flask(__name__)
 app.secret_key = 'moresecrets'
@@ -15,37 +15,60 @@ def display_form():
 def get_user_input():
     '''retrieves user input and stores in Session'''
 
-    data = {
+    form_inputs = {
         'from_currency': request.form.get('currency_1'),
         'to_currency': request.form.get('currency_2'),
         'amount': request.form.get('amount'),
-        'format': '1'
     }
-    session['data'] = data
+    session['form_inputs'] = form_inputs
     return redirect('/make_api_call/')
 
 
 @app.route('/make_api_call/', methods=['GET'])
 def fx_conversion():
     '''calls api and returns results which is saved in session'''
-    if 'data' in session:
-        data=session['data']
-        amount = data.get('amount')
-        from_currency = data.get('from_currency')
-        to_currency = data.get('to_currency')
-    if not all((amount,from_currency, to_currency)):
-        return 'incomplete data'
 
+    response=session['form_inputs']
+    amount = response.get('amount')
+    from_currency = response.get('from_currency')
+    to_currency =response.get('to_currency')
     result=make_api_call(amount,from_currency,to_currency)
-    if result:
-        session['result'] = result
-        return redirect('/render_results/')
-    else:
-        return 'API call failed'
+    status=result['success']
+
+
+    if status is False:
+        flash(f"error-{result['error']['type']}")
+        return redirect('/')
+    else :
+        session['result']=result
+        return redirect('/render_results')
 
 
 @app.route('/render_results/', methods=['GET', 'POST'])
 def show_results():
-    '''displays result of currency conversion and adds currency symbol '''
-    return display_result_page(session)
+    symbol=get_currency_symbol(session['result']['query']['to'])
+    amount=str(round(session['result']['result'],2))
 
+    return render_template('fx_output.html',symbol=symbol,amount=amount)
+
+
+
+
+
+
+#  {
+#   "info": {
+#     "quote": 149.058013,
+#     "timestamp": 1698693424
+#   },
+#   "privacy": "https://currencylayer.com/privacy",
+#   "query": {
+#     "amount": 100,
+#     "from": "USD",
+#     "to": "JPY"
+#   },
+#   "result": 14905.8013,
+#   "success": true,
+#   "terms": "https://currencylayer.com/terms"
+
+#  }
--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,4 @@
-from flask import render_template,flash
+from flask import flash,redirect
 import requests
 from forex_python.converter import CurrencyCodes
 
@@ -10,31 +10,17 @@ def make_api_call(amount, from_currency, to_currency):
     url = f'{base_url}{access_key}&from={from_currency}&to={to_currency}&amount={amount}&format=1'
 
     response = requests.get(url=url)
+    result = response.json()
+    return result
 
-    if response.status_code == 200:
-        return response.json()
-    return None
+def check_api_status(res,status):
+    if status is False:
+        flash(f"error-{result['error']['type']}")
+        return redirect('/')
+    # return result
+    # return 'all good'
 
-def get_exchange_value(session):
-    result = session.get('result')
-    if result:
-        try:
-            exchange = str(round(session['result']['result'], 2))
-        except KeyError:
-            # You can modify this to handle the missing key differently
-            return None
-        return exchange
-    return None
+
 
 def get_currency_symbol(currency_code):
     return c.get_symbol(currency_code.upper())
-
-def display_result_page(session):
-    symbol = get_currency_symbol(session['data']['to_currency'])
-    exchange = get_exchange_value(session)
-
-    if exchange is None:
-        flash('Invalid currency code: please re-enter', 'error')
-        return render_template('fx_input.html')
-
-    return render_template('fx_output.html', symbol=symbol, exchange=exchange)
